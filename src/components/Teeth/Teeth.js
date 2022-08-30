862250eed4efa8acec8dddd538b2d8c6fb35af62
Kirill <k.pirinen.rus@gmail.com>
2022-08-30 23:14:34 +0300

    teeth formula layout +

--- src/components/Teeth/Teeth.js
+++ src/components/Teeth/Teeth.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { teethKeys, teethMeta } from './paths-dict'
+import { teethKeysAdult, teethMetaAdult, teethKeysBaby, teethMetaBaby } from './paths-dict'
 import { Tooth } from './Tooth'
 
 export const Teeth = () => {
-  return teethKeys.map(tooth => <Tooth 
-    key={tooth}
-    toothNo={tooth}
-    paths={teethMeta[tooth].paths}
-    x={teethMeta[tooth].x}
-    y={teethMeta[tooth].y}
-    style={teethMeta[tooth].style}
-    textStyle={teethMeta[tooth].textStyle}
-  />)
+  const withBabyTeeth = true
+  return (
+    <>
+      {teethKeysAdult.map(tooth => <Tooth 
+        key={tooth}
+        toothNo={tooth}
+        paths={teethMetaAdult[tooth].paths}
+        x={teethMetaAdult[tooth].x}
+        y={teethMetaAdult[tooth].y}
+        style={teethMetaAdult[tooth].style}
+        textStyle={teethMetaAdult[tooth].textStyle}
+      />)}
+      {withBabyTeeth && teethKeysBaby.map(tooth => <Tooth 
+        key={tooth}
+        toothNo={tooth}
+        paths={teethMetaBaby[tooth].paths}
+        x={teethMetaBaby[tooth].x}
+        y={teethMetaBaby[tooth].y}
+        style={teethMetaBaby[tooth].style}
+        textStyle={teethMetaBaby[tooth].textStyle}
+      />)}
+    </>
+  )
 }
